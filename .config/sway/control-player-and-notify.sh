6b76158f1c0c853322a0dd5550d79de3ce2bf0ef
glbe <ethernet389@gmail.com>
2026-01-23 13:31:01 +0300

    refactor: add more info for player

--- .config/sway/control-player-and-notify.sh
+++ .config/sway/control-player-and-notify.sh
@@ -18,32 +18,38 @@ playerctl-configured() {
 
 if [ "$1" = "play-pause" ]; then
   playerctl-configured play-pause \
-    && notify-send-configured "Play-pause player" "$(playerctl-configured status)" \
+    && notify-send-configured "Play-pause player ($(playerctl-configured metadata --format '{{playerName}}'))" \
+      "$(playerctl-configured status)" \
     || notify-send-configured "Failed to play-pause player"
 
 elif [ "$1" = "next" ]; then
   playerctl-configured next \
-    && notify-send-configured "Play next on player" "$(playerctl-configured metadata --format '{{title}}')" \
+    && notify-send-configured "Play next on player ($(playerctl-configured metadata --format '{{playerName}}'))" \
+      "$(playerctl-configured metadata --format '{{title}}')" \
     || notify-send-configured "Failed to play previous"
 
 elif [ "$1" = "previous" ]; then
   playerctl-configured previous \
-    && notify-send-configured "Play previous on player" "$(playerctl-configured metadata --format '{{title}}')" \
+    && notify-send-configured "Play previous on player ($(playerctl-configured metadata --format '{{playerName}}'))" \
+      "$(playerctl-configured metadata --format '{{title}}')" \
     || notify-send-configured "Failed to play previous on player"
 
 elif [ "$1" = "stop" ]; then
   playerctl-configured stop \
-    && notify-send-configured "Stop player" "$(playerctl-configured metadata --format '{{title}}')" \
+    && notify-send-configured "Stop player ($(playerctl-configured metadata --format '{{playerName}}'))" \
+      "$(playerctl-configured metadata --format '{{title}}')" \
     || notify-send-configured "Failed to stop player"
 
 elif [ "$1" = "increase-level" ]; then
   playerctl-configured volume 0.05+ \
-    && notify-send-configured "Increase player volume by 5%" "$(playerctl-configured volume)" \
+    && notify-send-configured "Increase player ($(playerctl-configured metadata --format '{{playerName}}')) volume by 5%" \
+      "$(playerctl-configured volume)" \
     || notify-send-configured "Failed to increase player volume"
 
 elif [ "$1" = "decrease-level" ]; then
   playerctl-configured volume 0.05- \
-    && notify-send-configured "Decrease player volume by 5%" "$(playerctl-configured volume)" \
+    && notify-send-configured "Decrease player ($(playerctl-configured metadata --format '{{playerName}}')) volume by 5%" \
+      "$(playerctl-configured volume)" \
     || notify-send-configured "Failed to increase player volume"
 
 
