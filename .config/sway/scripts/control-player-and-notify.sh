1e67ac092c1957a10a36947244d30c267189bb29
glbe <ethernet389@gmail.com>
2026-03-26 19:36:06 +0300

    refactor: override some scripts

--- .config/sway/scripts/control-player-and-notify.sh
+++ .config/sway/scripts/control-player-and-notify.sh
@@ -1,57 +1,75 @@
 #!/usr/bin/env bash
 
-PLAYERCTL_OPTS="--player=vlc,mpd,%any,firefox"
 NOTIFICATION_LIFETIME=2000
 PLAYER_CONTROL_ID="string:x-canonical-private-synchronous:player_control"
 
-notify-send-configured() {
-  notify-send \
-    -t $NOTIFICATION_LIFETIME \
-    -h $PLAYER_CONTROL_ID \
-    "$@"
+
+notify_send_configured() {
+	notify-send \
+		-t $NOTIFICATION_LIFETIME \
+		-h $PLAYER_CONTROL_ID \
+		"$@"
 }
 
-playerctl-configured() {
-  playerctl $PLAYERCTL_OPTS "$@"
+playerctl_configured() {
+	playerctl --player=vlc,mpd,%any,firefox "$@"
+}
+
+playerctl_data() {
+	playerctl_configured metadata --format "{{$1}}"
 }
 
 
-if [ "$1" = "play-pause" ]; then
-  playerctl-configured play-pause \
-    && notify-send-configured "Play-pause player ($(playerctl-configured metadata --format '{{playerName}}'))" \
-      "$(playerctl-configured status)" \
-    || notify-send-configured "Failed to play-pause player"
+declare -a playerctl_args
+declare -a good_msg_args
+declare -a bad_msg_args
 
-elif [ "$1" = "next" ]; then
-  playerctl-configured next \
-    && notify-send-configured "Play next on player ($(playerctl-configured metadata --format '{{playerName}}'))" \
-      "$(playerctl-configured metadata --format '{{title}}')" \
-    || notify-send-configured "Failed to play previous"
+case "$1" in
+	play-pause)
+		playerctl_args=( play-pause )
+		good_msg_args=( "Play-pause player ($(playerctl_data playerName))" )
+		bad_msg_args=( "Failed to play-pause player ($(playerctl_data playerName))" )
+		;;
 
-elif [ "$1" = "previous" ]; then
-  playerctl-configured previous \
-    && notify-send-configured "Play previous on player ($(playerctl-configured metadata --format '{{playerName}}'))" \
-      "$(playerctl-configured metadata --format '{{title}}')" \
-    || notify-send-configured "Failed to play previous on player"
+	next)
+		playerctl_args=( next )
+		good_msg_args=( "Play next on player ($(playerctl_data playerName))" "$(playerctl_data title)" )
+		bad_msg_args=( "Failed to play next player ($(playerctl_data playerName))" )
+		;;
 
-elif [ "$1" = "stop" ]; then
-  playerctl-configured stop \
-    && notify-send-configured "Stop player ($(playerctl-configured metadata --format '{{playerName}}'))" \
-      "$(playerctl-configured metadata --format '{{title}}')" \
-    || notify-send-configured "Failed to stop player"
+	previous)
+		playerctl_args=( previous )
+		good_msg_args=( "Play previous on player ($(playerctl_data playerName))" "$(playerctl_data title)" )
+		bad_msg_args=( "Failed to play previous player ($(playerctl_data playerName))" )
+		;;
 
-elif [ "$1" = "increase-level" ]; then
-  playerctl-configured volume 0.05+ \
-    && notify-send-configured "Increase player ($(playerctl-configured metadata --format '{{playerName}}')) volume by 5%" \
-      "$(playerctl-configured volume)" \
-    || notify-send-configured "Failed to increase player volume"
+	stop)
+		playerctl_args=( stop )
+		good_msg_args=( "Stop player ($(playerctl_data playerName))" "$(playerctl_data title)" )
+		bad_msg_args=( "Failed to stop player ($(playerctl_data playerName))" )
+		;;
 
-elif [ "$1" = "decrease-level" ]; then
-  playerctl-configured volume 0.05- \
-    && notify-send-configured "Decrease player ($(playerctl-configured metadata --format '{{playerName}}')) volume by 5%" \
-      "$(playerctl-configured volume)" \
-    || notify-send-configured "Failed to decrease player volume"
+	increase-level)
+		playerctl_args=( volume 0.05+ )
+		good_msg_args=( "Increase player ($(playerctl_data playerName)) volume by 5%" "$(playerctl_data volume)" )
+		bad_msg_args=( "Failed increase player volume ($(playerctl_data playerName))" )
+		;;
+
+	decrease-level)
+		playerctl_args=( volume 0.05- )
+		good_msg_args=( "Decrease player ($(playerctl_data playerName)) volume by 5%" "$(playerctl_data volume)" )
+		bad_msg_args=( "Failed decrease player volume ($(playerctl_data playerName))" )
+		;;
+
+	*)
+		echo "Unknown command" >&2
+		exit 1
+		;;
+esac
 
 
-
+if playerctl_configured "${playerctl_args[@]}"; then
+	notify_send_configured "${good_msg_args[@]}"
+else
+	notify_send_configured "${bad_msg_args[@]}"
 fi
--- .config/sway/scripts/set-brightness-and-notify.sh
+++ .config/sway/scripts/set-brightness-and-notify.sh
@@ -3,43 +3,62 @@
 NOTIFICATION_LIFETIME=2000
 BRIGHTNESS_CONTROL_ID="string:x-canonical-private-synchronous:brightness_control"
 
+
 brightness_get_percentage() {
-  brightnessctl get | awk '{print int($1 * 100 / '$(brightnessctl max)')}'
+	brightnessctl get \
+		| awk -v max_bright="$(brightnessctl max)" '{print int($1 * 100 / max_bright)}'
 }
 
-notify-send-configured() {
-  notify-send \
-    -t $NOTIFICATION_LIFETIME \
-    -h $BRIGHTNESS_CONTROL_ID \
-    "$@"
+notify_send_configured() {
+	notify-send \
+		-t $NOTIFICATION_LIFETIME \
+		-h $BRIGHTNESS_CONTROL_ID \
+		"$@"
 }
 
 
-if [ "$1" = "increase" ]; then
-  brightnessctl set 5%+ \
-    && notify-send-configured "Increase brightness" "$(brightness_get_percentage)%" \
-    || notify-send-configured "Failed to increase brightness"
+declare -a args
+declare -a good_msg_args
+declare -a bad_msg_args
 
-elif [ "$1" = "decrease" ]; then
-  brightnessctl set 5%- \
-    && notify-send-configured "Decrease brightness" "$(brightness_get_percentage)%" \
-    || notify-send-configured "Failed to decrease brightness"
+case "$1" in
+	increase)
+		args=( set 5%+ )
+		good_msg_args=( "Increase brightness" "$(brightness_get_percentage)%" )
+		bad_msg_args=( "Failed to increase brightness" )
+		;;
 
-elif [ "$1" = "toggle" ]; then
-  MIN_VALUE=1 
-  MAX_VALUE=$(brightnessctl max)
+	decrease)
+		args=( set 5%- )
+		good_msg_args=( "Decrease brightness" "$(brightness_get_percentage)%" )
+		bad_msg_args=( "Failed to decrease brightness" )
+		;;
 
-  current_value=$(brightnessctl get)
+	toggle)
+		min_value=1
+		max_value=$(brightnessctl max)
+		current_value=$(brightnessctl get)
 
-  if [ $current_value = $MIN_VALUE ]; then
-    brightnessctl set $MAX_VALUE \
-      && notify-send-configured "Maximize brightness" "$(brightness_get_percentage)%" \
-      || notify-send-configured "Failed to maximize brightness"
+		if [[ $current_value = "$min_value" ]]; then
+			args=( set "$max_value" )
+			good_msg_args=( "Maximize brightness" "$(brightness_get_percentage)%" )
+			bad_msg_args=( "Failed to maximize brightness" )
+		else
+			args=( set "$min_value" )
+			good_msg_args=( "Minimize brightness" "$(brightness_get_percentage)%" )
+			bad_msg_args=( "Failed to minimize brightness" )
+		fi
+		;;
 
-  else
-    brightnessctl set $MIN_VALUE \
-      && notify-send-configured "Minimize brightness" "$(brightness_get_percentage)%" \
-      || notify-send-configured "Failed to minimize brightness"
-  fi
+	*)
+		echo "Unknown command" >&2
+		exit 1
+		;;
+esac
 
+
+if brightnessctl "${args[@]}"; then
+	notify_send_configured "${good_msg_args[@]}"
+else
+	notify_send_configured "${bad_msg_args[@]}"
 fi
